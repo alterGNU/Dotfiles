b9adf2a3d94231fe06d1815494c43fade02153bc
alterGNU <alterGNU@gmail.com>
2024-12-01 08:45:41 +0100

    fcts on pause

--- install.sh
+++ install.sh
@@ -1,41 +1,89 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 
 # ============================================================================================================
-# INSTALL
-# install my configuration, making backup folder with old dotfiles before replacing them by links
+# INSTALL SCRIPT
+# install my conf.: make backup folder with actual dotfiles before replacing them with sym-links to my dotfiles
+# - after `git clone https://github.com/alterGNU/Dotfiles.git` just do `./Dorfiles/install.sh`
+# - with curl : `sh -c "$(curl -fsSL https://raw.githubusercontent.com/alterGNU/Dotfiles/refs/heads/main/install.sh)`
+# - with wget : `sh -c "$(wget -qO- https://raw.githubusercontent.com/alterGNU/Dotfiles/refs/heads/main/install.sh)`
+# - with fetch: `sh -c "$(fetch -o - https://raw.githubusercontent.com/alterGNU/Dotfiles/refs/heads/main/install.sh)`
 # 
 # PRE-REQUIS
 # - Dotfiles submodules uptodate :`git clone --recurse-submodules -j8 git@github.com:alterGNU/Dotfiles.git`
 # - zsh already install (Oh my zsh too)
+#
+# GOOD-TO-KNOW
+# - Dotfiles project can be clone anywhere, this script will create symbolic-links so that the dotfiles work.
+# - VAR-ENV : DOTPATH (set at "" by default), is the path to the Dotfiles folder
+#
+# TODO :
+# - [ ] Add pre-requis test (zsh + oh-my-zsh + vim + git) cf Ubuntu_install install script
+# - [ ] Usemode with git-clone
+# - [ ] Usemode with curl or wget
+# - [ ] Add ASCI art function (start install, then chapter:zshrc install, vim, git )
 # ============================================================================================================
  
-# =[ VAR ]====================================================================================================
-BCK="${HOME}/backups"
-FLD="${BCK}/$(date +%Y_%m_%d.%Hh%Mm%Ss)"
-DOTPATH=$(dirname $(realpath ${0}))
-ACTPWD=${pwd}
+# ============================================================================================================
+# VAR
+# ============================================================================================================
+# =[ COLORS ]=================================================================================================
+R="\033[0;31m"                                   # START RED
+V="\033[0;32m"                                   # START GREEN
+M="\033[0;33m"                                   # START BROWN
+Y="\033[0;93m"                                   # START YELLOW
+B="\033[0;36m"                                   # START BLUE
+E="\033[0m"                                      # END color balise
+ 
+# =[ PATH ]===================================================================================================
+BCK="${HOME}/backups"                            # Path of the backup folder
+FLD="${BCK}/$(date +%Y_%m_%d.%Hh%Mm%Ss)"         # Name of the backup folder
+DOTPATH=$(dirname $(realpath ${0}))              # Path of the Dotfile folder
 
-# =[ FUNCTIONS ]==============================================================================================
+# ============================================================================================================
+# FUNCTIONS
+# ============================================================================================================
+ 
+# -[ CREATE_BCKUP_FOLDER() ]----------------------------------------------------------------------------------
+# create a backup folder if not already created
 create_bckup_folder() { [[ ! -d ${FLD}/${1} ]] && mkdir -p ${FLD}/${1} ; }
+# -[ EXEC_ANIM() ]--------------------------------------------------------------------------------------------
+# print animation in frontground while cmd exec in background the print returns.
+exec_anim()
+{
+    local frames=( ▹▹▹▹▹ ▸▹▹▹▹ ▹▸▹▹▹ ▹▹▸▹▹ ▹▹▹▸▹ ▹▹▹▹▸ ▹▹▹▹▹ ▹▹▹▹▹ ▹▹▹▹▹ ▹▹▹▹▹ ▹▹▹▹▹ ▹▹▹▹▹ ▹▹▹▹▹ )
+    local delay=0.15
+    local cmd="${@}"
+    local tmpfile=$(mktemp "${TMPDIR:-/tmp}/exec_anim_${cmd%% *}_XXXXXX")
+    trap '[[ -f "${tmpfile}" ]] && rm -f "${tmpfile}"' EXIT RETURN
+    ${cmd} > "${tmpfile}" 2>&1 &
+    local pid=${!}
+    while kill -0 ${pid} 2>/dev/null; do
+        for frame in "${frames[@]}"; do printf "\r${frame}" && sleep ${delay} ; done
+    done
+    printf "\r" && wait ${pid}
+    local exit_code=${?}
+    printf "\r" && cat "${tmpfile}"
+    return ${exit_code}
+}
 
 # ============================================================================================================
 # MAIN
 # ============================================================================================================
  
-# -[ INSTALL ZSHRC ]------------------------------------------------------------------------------------------
-sed -i "/^export DOTPATH=/c\export DOTPATH=${DOTPATH}" ${DOTPATH}/zshrc
-[[ -h ~/.zshrc ]] && rm ${HOME}/.zshrc
-[[ -f ~/.zshrc ]] && { create_bckup_folder && mv ${HOME}/.zshrc ${FLD}/zshrc ; }
-ln -s ${DOTPATH}/zshrc ${HOME}/.zshrc
-
-# -[ INSTALL GITCONFIG ]--------------------------------------------------------------------------------------
-[[ -h ~/.gitconfig ]] && rm ${HOME}/.gitconfig
-[[ -f ~/.gitconfig ]] && { create_bckup_folder && mv ${HOME}/.gitconfig ${FLD}/gitconfig ; }
-ln -s ${DOTPATH}/gitconfig ${HOME}/.gitconfig
-
-# -[ INSTALL VIM ]--------------------------------------------------------------------------------------------
-[[ -h ~/.vim ]] && rm ${HOME}/.vim
-[[ -d ~/.vim ]] && { create_bckup_folder && mv ${HOME}/.vim ${FLD}/vim ; }
-ln -s ${DOTPATH}/vim ${HOME}/.vim
-echo -e "\n" | vim -c "PlugInstall" -c "qa" > /dev/null 2>&1
-echo -e "\n" | vim -c "PlugUpdate" -c "qa" > /dev/null 2>&1
+## -[ INSTALL ZSHRC ]------------------------------------------------------------------------------------------
+#sed -i "/^export DOTPATH=/c\export DOTPATH=${DOTPATH}" ${DOTPATH}/zshrc
+#[[ -h ~/.zshrc ]] && rm ${HOME}/.zshrc
+#[[ -f ~/.zshrc ]] && { create_bckup_folder && mv ${HOME}/.zshrc ${FLD}/zshrc ; }
+#ln -s ${DOTPATH}/zshrc ${HOME}/.zshrc
+#
+## -[ INSTALL GITCONFIG ]--------------------------------------------------------------------------------------
+#[[ -h ~/.gitconfig ]] && rm ${HOME}/.gitconfig
+#[[ -f ~/.gitconfig ]] && { create_bckup_folder && mv ${HOME}/.gitconfig ${FLD}/gitconfig ; }
+#ln -s ${DOTPATH}/gitconfig ${HOME}/.gitconfig
+#
+## -[ INSTALL VIM ]--------------------------------------------------------------------------------------------
+#[[ -h ~/.vim ]] && rm ${HOME}/.vim
+#[[ -d ~/.vim ]] && { create_bckup_folder && mv ${HOME}/.vim ${FLD}/vim ; }
+#ln -s ${DOTPATH}/vim ${HOME}/.vim
+#echo -e "\n" | vim -c "PlugInstall" -c "qa" > /dev/null 2>&1
+#echo -e "\n" | vim -c "PlugUpdate" -c "qa" > /dev/null 2>&1
--- zshrc
+++ zshrc
@@ -9,14 +9,15 @@
 # ========================================================================================================== #
  
 # =[ EXPORTS ]================================================================================================
-# ADD funckeck to PATH
 export ZSH="$HOME/.oh-my-zsh"
-export PATH="$HOME/.local/bin:${PATH}"
-export PATH=/home/altergnu/.local/funcheck/host:$PATH
-export CLE=/media/${USERNAME}/Lexar/
 export VIMRC=/home/${USERNAME}/.vim/vimrc
+# -[ DOTPATH ]------------------------------------------------------------------------------------------------
 export DOTPATH=/home/altergnu/Projects/Dotfiles
-[[ -d ${DOTPATH}/fcts ]] && export FPATH=${DOTPATH}/fcts:$FPATH
+[[ -d ${DOTPATH}/fcts ]] && export FPATH=${DOTPATH}/fcts:$FPATH    # ADD $DOTPATH to $FPATH
+# -[ PATH ]---------------------------------------------------------------------------------------------------
+export PATH="$HOME/.local/bin:${PATH}"                             # ADD ~/.local/bin to $PATH
+export PATH=/home/altergnu/.local/funcheck/host:$PATH              # ADD funckeck to $PATH
+export CLE=/media/${USERNAME}/Lexar/
 
 # =[ ZSH SETTINGS ]===========================================================================================
 # Set name of the theme to load --- if set to "random", it will load a random theme each time oh-my-zsh is 
@@ -34,24 +35,40 @@ plugins=(git)
 source $ZSH/oh-my-zsh.sh
 
 # =[ FUNCTIONS ]==============================================================================================
-autoload -Uz gac         # git add commit
-autoload -Uz gdft        # git difftool
-autoload -Uz gus         # git update submodule
-autoload -Uz guw         # git update wiki
-autoload -Uz update_gpw  # update folder Github-Projects-Wikis
+#autoload -Uz git_add_all_commit_push   # `gaa && gc -m"arg1" ; git push "arg2" "arg3"`
+#autoload -Uz git_difftool              # if no arg:`git difftool $PWD`, else `git difftool *argV`
+#autoload -Uz git_update_submodule      #
+#autoload -Uz git_add_wiki_as_submodule
+#autoload -Uz git_update_wiki
+#autoload -Uz update_GPW                # update folder Github-Projects-Wikis
+
+# -[ GET_TASK_UUID() ]----------------------------------------------------------------------------------------
+# TaskWarrior Get: task _get <ID> <attributs> in clipboard
+twg(){ task _get $(echo ${@} | tr ' ' '.') | tee >(xsel -ib) ; }
+# TaskWarrior Export: task <ID> export in clipboard
+twe(){ task ${1} export | tee >(xsel -ib) ; }
 
 # =[ ALIAS ]==================================================================================================
-alias francinette=/home/altergnu/francinette/tester.sh
-alias paco=/home/altergnu/francinette/tester.sh
-alias ccw="cc -Wall -Wextra -Werror -lbsd"
+# -[ VIM ]----------------------------------------------------------------------------------------------------
 alias wiki='[[ -d ${HOME}/Wiki ]] && vim ~/Wiki/index.md || echo "alias not available here"'
-alias notes='[[ -d ${HOME}/Notes ]] && vim ~/Notes/index.md || echo "alias not available here"'
 alias diary='[[ -d ${HOME}/Wiki/diary ]] && vim ~/Wiki/diary/diary.md || echo "alias not available here"'
-alias todo='[[ -d ${HOME}/Todo ]] && vim ~/Todo/index.md || echo "alias not available here"'
+alias todo='[[ -f ${HOME}/Wiki/Todo/index.md ]] && vim ${HOME}/Wiki/Todo/index.md || echo "alias not available here"'
+alias idea='[[ -f ${HOME}/Wiki/Todo/Id/index.md ]] && vim ${HOME}/Wiki/Todo/Id/index.md || echo "alias not available here"'
+alias question='[[ -f ${HOME}/Wiki/Todo/list_of_Question_tickets.md ]] && vim ${HOME}/Wiki/Todo/list_of_Question_tickets.md || echo "alias not available here"'
+alias howto='[[ -f ${HOME}/Wiki/Todo/list_of_How_to_tickets.md ]] && vim ${HOME}/Wiki/Todo/list_of_How_to_tickets.md || echo "alias not available here"'
+alias fixit='[[ -f ${HOME}/Wiki/Todo/list_of_Fix_it_tickets.md ]] && vim ${HOME}/Wiki/Todo/list_of_Fix_it_tickets.md || echo "alias not available here"'
 alias gpw='[[ -d ${HOME}/GPW ]] && vim ~/GPW/Home.md || echo "alias not available here"'
 alias wikis=gpw
-alias gsu=gus
-alias gpwu=update_gpw
+# -[ CMDS ]---------------------------------------------------------------------------------------------------
+alias ccw="cc -Wall -Wextra -Werror -lbsd"
 alias is_a_gitrepo="git rev-parse --is-inside-work-tree &>/dev/null && echo yes || echo no"
 alias gitrepo_is_uptodate="git diff-index --quiet HEAD -- 2> /dev/null && echo yes || echo no"
-
+# -[ SCRIPTS ]------------------------------------------------------------------------------------------------
+alias francinette=/home/altergnu/francinette/tester.sh
+alias paco=/home/altergnu/francinette/tester.sh
+# -[ FCTS ]---------------------------------------------------------------------------------------------------
+#alias gaacp=git_add_all_commit_push
+#alias gdft=git_difftool
+#alias gus=git_update_submodule
+#alias guw=git_update_wiki
+#alias gugpw=git_update_GPW
